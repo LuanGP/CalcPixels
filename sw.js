4f745eb2d46014d3a273bf2265d4085cde552db9
LuanGP <luan.gp2009@gmail.com>
2025-07-22 21:24:07 -0300

    1.5.5

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'calcpixels-v1.5.1';
+const urlsToCache = [
+  '/CalcPixels/',
+  '/CalcPixels/index.html',
+  '/CalcPixels/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+}); 
