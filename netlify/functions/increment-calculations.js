5e106456ce7ccfca6890a0fd1e12c0bf0d57e223
LuanGP <luan.gp2009@gmail.com>
2025-07-23 21:12:27 -0300

    Revert "vai"
    This reverts commit 95c1c84847158ce52333c0020f476e96ca4255d8.

--- netlify/functions/increment-calculations.js
+++ netlify/functions/increment-calculations.js
@@ -1,19 +1,6 @@
 const { Client } = require('pg');
 
 exports.handler = async function(event, context) {
-  // Suporte ao CORS preflight
-  if (event.method === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-      },
-      body: ''
-    };
-  }
-
   const client = new Client({ connectionString: process.env.NETLIFY_DATABASE_URL });
   await client.connect();
 
@@ -23,12 +10,6 @@ exports.handler = async function(event, context) {
 
   return {
     statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-    },
     body: JSON.stringify(res.rows[0])
   };
 }; 
